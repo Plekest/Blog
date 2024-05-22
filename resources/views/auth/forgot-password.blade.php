7b36118417445eddf0b9f99fe2ac01e78c858d76
Pedro Teles <phtf.pedro12@gmail.com>
2024-05-22 16:33:36 -0300

    redirect view forgot-password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
+            <x-primary-button href="{{ url('/login') }}">
                 {{ __('Enviar Link') }}
             </x-primary-button>
         </div>
